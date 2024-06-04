3d3b3ca1c1f15ef374cbf62d73bf5a407b08e77b
Luciclei Oliveira <134169478+luciclei@users.noreply.github.com>
2024-06-03 21:30:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -40,3 +40,26 @@ var_dump ($c); echo"<br>";
 var_dump ($d); echo"<br>";
 var_dump ($e);  echo"<br>";
 ?>
+
+<?php
+$a = array ("volvo", "BMW", "Toyota"); //array indexado 
+//$b = array ("Peter" ="30", "Joao" >= "40", "Carlos" >="50");
+//array associativo
+
+$a = (object) $a;
+$b = (object) $b;
+echo"<br>";
+
+var_dump($a);echo "<br>";
+var_dump($b);echo "<br>";
+
+?>
+
+<?php
+echo (pi());
+echo "<br>";
+echo (min(0, 20, 130, -10,-230)). "<br>";
+echo (max(0, 25, -230, 100, -530)). "<br>";
+echo (sqrt(64)) . "<br>";
+echo (round(0.60)) . "<br>";
+?>
